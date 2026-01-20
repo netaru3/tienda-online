0e8e48a313c01b19613b3614205450595cec347d
Aarón <netaru3@gmail.com>
2026-01-20 15:43:07 -0300

    Update mongo-productos.js

--- mongo-productos.js
+++ mongo-productos.js
@@ -8,7 +8,7 @@ catch(error){console.log("error:",error)}
 let esquema=new mongoose.Schema({
  producto_nombre: String,
  producto_descripcion: String,
- producto_imagen: String,
+ producto_imagen: Array,
  producto_precio: String,
  producto_stock: String,
  producto_id: Number,
