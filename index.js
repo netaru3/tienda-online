5d4695b273c9f4e557ffee63f3dd04a6c4464bf2
Aarón <netaru3@gmail.com>
2026-01-28 23:11:35 -0300

    Fix product stock check in webhook handling

--- index.js
+++ index.js
@@ -469,7 +469,7 @@ app.post("/webhook", async function(req, res) {
                 let producto= await log_products.find({producto_id: paymentInfo.metadata.id})
                 console.log("producto del webhook:",producto)
         
-    if(Number(producto.producto_stock)>0){let nuevoproducto= await log_products.findOneAndUpdate({producto_id: producto.producto_id},{producto_stock: `${Number(producto.producto_stock)-1}`})
+    if(Number(producto[0].producto_stock)>0){let nuevoproducto= await log_products.findOneAndUpdate({producto_id: producto[0].producto_id},{producto_stock: `${Number(producto[0].producto_stock)-1}`})
    }}
    
                 
